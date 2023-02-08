d4e59ce82820d95f115cd76a86674749f17e8409
stakbucks <dhassidu11@gmail.com>
2023-02-08 20:12:25 +0900

    중복확인 버튼, 비밀번호 확인 입력란 추가

--- src/components/forms/SignUpForm.tsx
+++ src/components/forms/SignUpForm.tsx
@@ -5,12 +5,12 @@ import { Input, Label } from "./FormStyles";
 const Form = styled.form`
   position: relative;
   width: calc((408 / 1920) * 100vw);
-  height: calc((717 / 1080) * 100vh);
+  height: calc((801 / 1080) * 100vh);
 `;
 
 const Title = styled.div`
   width: 100%;
-  height: calc((51 / 717) * 100%);
+  height: calc((51 / 801) * 100%);
   position: absolute;
   left: 28.43%;
   right: 28.19%;
@@ -27,95 +27,67 @@ const Title = styled.div`
 const NameWrapper = styled.div`
   width: 100%;
   position: absolute;
-  right: 0.12%;
-  left: 0;
-  top: 16.88%;
-  bottom: 80.47%;
+  left: 0%;
+  right: 0%;
+  top: 15.11%;
+  bottom: 74.91%;
 `;
 
 const IdWrapper = styled.div`
   width: 100%;
   position: absolute;
-  left: 0;
-  right: 0.12%;
-  top: 31.52%;
-  bottom: 65.83%;
-  span {
-    font-size: 12px;
-    margin-left: 10px;
-  }
+  left: 0%;
+  right: 0%;
+  top: 28.21%;
+  bottom: 58.43%;
+`;
+
+const IdInputWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  position: relative;
 `;
 
 const PasswordWrapper = styled.div`
   width: 100%;
   position: absolute;
   left: 0%;
-  right: 0.12%;
-  top: 49.93%;
-  bottom: 47.42%;
-  span {
-    font-size: 12px;
-    margin-left: 10px;
-  }
+  right: 0%;
+  top: 44.69%;
+  bottom: 31.46%;
 `;
+
+const VerifyPasswordWrapper = styled.div`
+  width: 100%;
+  position: absolute;
+  left: 0%;
+  right: 0%;
+  top: 59.55%;
+  bottom: 34.83%;
+`;
+
 const EmailWrapper = styled.div`
   width: 100%;
   position: absolute;
   left: 0%;
-  right: 0.12%;
-  top: 68.34%;
-  bottom: 29.01%;
-  span {
-    font-size: 12px;
-    margin-left: 10px;
-  }
-`;
-const LoginBtn = styled.button`
-  width: 100%;
-  height: calc((60 / 587) * 100%);
-  position: absolute;
-  left: 0%;
-  right: 0.12%;
-  top: 62.79%;
-  bottom: 26.63%;
-  background: #000000;
-  border-radius: 40px;
-  color: white;
-  cursor: pointer;
-`;
-
-const FindIdPassword = styled.div`
-  width: calc((167.5 / 408.5) * 100%);
-  height: calc((25 / 567) * 100%);
-  position: absolute;
-  left: 59%;
   right: 0%;
-  top: 77.78%;
-  bottom: 17.81%;
-  font-weight: 400;
-  line-height: 23px;
-  font-size: 12px;
-`;
-const FindId = styled.span`
-  cursor: pointer;
-  text-decoration: underline;
-`;
-const FindPassword = styled.span`
-  cursor: pointer;
-  text-decoration: underline;
+  top: 71.66%;
+  bottom: 14.98%;
 `;
 
-const CofirmBtn = styled.div`
+const CofirmBtn = styled.span`
   position: absolute;
-  width: calc((55 / 408) * 100%);
-  height: calc((20 / 717) * 100%);
-  font-size: 15px;
-  right: 0px;
-  bottom: calc(calc((45 / 1080) * 100%) / 2);
+  right: 10px;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 26px;
   cursor: pointer;
-  span {
-    margin: 0 3px;
-  }
+`;
+
+const Notice = styled.span`
+  font-size: 12px;
+  margin-left: 10px;
 `;
 
 const SignUpBtn = styled.button`
@@ -124,7 +96,6 @@ const SignUpBtn = styled.button`
   position: absolute;
   left: 20.89%;
   right: 24.75%;
-  top: 91.63%;
   bottom: 0%;
   background: #000000;
   border-radius: 40px;
@@ -142,21 +113,25 @@ function SignUpForm() {
       </NameWrapper>
       <IdWrapper>
         <Label>아이디</Label>
-        <Input type="text" />
-
-        <span>8~20자의 영문 대/소문자, 숫자, 특수문자로 설정해주세요.</span>
+        <IdInputWrapper>
+          <Input type="text" />
+          <CofirmBtn>| 확인</CofirmBtn>
+        </IdInputWrapper>
+        <Notice>8~20자의 영문 대/소문자, 숫자, 특수문자로 설정해주세요.</Notice>
       </IdWrapper>
       <PasswordWrapper>
         <Label>비밀번호</Label>
         <Input type="password" />
-
-        <span>10글자 이하로 비밀번호를 입력해주세요.</span>
+        <Notice>10글자 이하로 비밀번호를 입력해주세요.</Notice>
       </PasswordWrapper>
-      <EmailWrapper>
-        <Label>비밀번호</Label>
+      <VerifyPasswordWrapper>
         <Input type="password" />
-
-        <span>계정을 찾으실 경우 필요한 정보입니다.</span>
+        <Notice>비밀번호를 다시 한 번 입력해주세요.</Notice>
+      </VerifyPasswordWrapper>
+      <EmailWrapper>
+        <Label>이메일</Label>
+        <Input type="email" />
+        <Notice>계정을 찾으실 경우 필요한 정보입니다.</Notice>
       </EmailWrapper>
       <SignUpBtn>회원가입하기</SignUpBtn>
     </Form>

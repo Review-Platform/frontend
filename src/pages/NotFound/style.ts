4b31206c779814d8148f9fce2ef4503d2183814e
stakbucks <dhassidu11@gmail.com>
2023-05-03 14:22:50 +0900

    [feat] 잘못된 경로 errorElement인 NotFound 구현

---
+++ src/pages/NotFound/style.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+export const Title = styled.h1`
+  color: var(--mainColor);
+  font-size: 32px;
+  font-weight: 700;
+  margin-top: 50px;
+  margin-left: 40px;
+`;

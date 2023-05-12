302a2f53c121e13bc59e4982b9a1cedb6f1286fa
stakbucks <dhassidu11@gmail.com>
2023-05-12 21:14:27 +0900

    [feat] NotFound 페이지 완성

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,5 +1,15 @@
 import * as S from "./style";
 function NotFound() {
-  return <S.Title>404 NOT FOUND</S.Title>;
+  return (
+    <S.Layout>
+      <S.BgImg src="/images/notFoundBg.png" />
+      <S.Title>404 {"\n"} NOT FOUND</S.Title>
+      <S.Row1>페이지를 찾을 수 없습니다.</S.Row1>
+      <S.Row2>
+        페이지가 존재하지 않거나, 사용할 수 없는 페이지입니다.{"\n"}입력하신
+        주소가 정확한지 다시 한 번 확인해주세요.
+      </S.Row2>
+    </S.Layout>
+  );
 }
 export default NotFound;
--- src/pages/NotFound/style.ts
+++ src/pages/NotFound/style.ts
@@ -1,8 +1,43 @@
 import styled from "styled-components";
-export const Title = styled.h1`
+export const Layout = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+`;
+export const BgImg = styled.img`
+  width: 901px;
+  height: 436px;
+  position: absolute;
+  top: 100px;
+  left: calc(50vw - 450px);
+  z-index: -1;
+  opacity: 0.4;
+`;
+export const Title = styled.div`
   color: var(--mainColor);
-  font-size: 32px;
+  width: 480px;
+  height: auto;
+  font-weight: 800;
+  font-size: 80px;
+  white-space: pre-line;
+  text-align: center;
+  margin-top: 80px;
+`;
+export const Row1 = styled.div`
+  margin-top: 48px;
   font-weight: 700;
-  margin-top: 50px;
-  margin-left: 40px;
+  font-size: 50px;
+  color: #e76100;
+`;
+export const Row2 = styled.div`
+  font-weight: 700;
+  font-size: 30px;
+  line-height: 46px;
+  color: #e76100;
+  white-space: pre-line;
+  text-align: center;
+  margin-top: 52px;
 `;
